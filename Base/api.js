0bb7d7a0be4bfe97865dd9836d1351996d70a801
Buyan <buyan.cipherlabz@gmail.com>
2026-03-01 20:36:53 +0530

    .

--- Base/api.js
+++ Base/api.js
@@ -1,5 +1,5 @@
 // const BASE_URL = 'https://apexqa.alevelclothing.lk/api';
-const BASE_URL = "http://qikcellbackend.clovesis.com/api";
+const BASE_URL = "https://qikcellbackend.clovesis.com/api";
 //const BASE_URL = "https://localhost:44352/api";
 
 export default BASE_URL;
